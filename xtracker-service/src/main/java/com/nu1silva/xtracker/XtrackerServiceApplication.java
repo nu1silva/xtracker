737c4c8e1c25f405a70d9b11a9c8849ddd50d083
nu1silva <nu1silva@gmail.com>
2021-01-05 22:58:05 +0100

    added initial boilerplate project

---
+++ xtracker-service/src/main/java/com/nu1silva/xtracker/XtrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nu1silva.xtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtrackerServiceApplication.class, args);
+	}
+
+}
---
+++ xtracker-service/src/test/java/com/nu1silva/xtracker/XtrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nu1silva.xtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
